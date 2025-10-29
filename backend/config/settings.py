70b60e76b4eb09d87d1e43d92c31198f9cc86114
Daniel7303 <danieleistien@gmail.com>
2025-10-29 17:46:48 +0100

    fixed database settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,9 +82,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
-        conn_max_age=600,
-        conn_max_age_func=lambda: 600
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
     )
 }
 
